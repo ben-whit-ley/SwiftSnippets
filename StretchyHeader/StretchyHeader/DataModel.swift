ff36bda910f237809e641d7e18d950b3526cc3e6
Ben Whitley <ben@dogonahorse.com>
2017-07-20 21:11:25 -0400

    stretchy header project added

---
+++ StretchyHeader/StretchyHeader/DataModel.swift
@@ -0,0 +1,35 @@
+//
+//  DataModel.swift
+//  EmbeddedCollectionView
+//
+//  Created by Ben Whitley on 7/19/17.
+//  Copyright © 2017 Ben Whitley. All rights reserved.
+//
+
+import Foundation
+
+let dataModel = DataModel()
+
+class DataModel {
+    let watch = [
+        "hero image": "apple watch hero",
+        "title": "Apple Watch",
+        "description": "Stay in touch like never before. Receive and respond to notiﬁcations in an instant. Track your daily activity. Control your music using only your voice.",
+        "section name": "Technical Specs",
+        "subtitle 1": "Color:",
+        "info 1": "Stainless Steel, Black Stainless Steel",
+        "subtitle 2": "Features:",
+        "info 2": "Stainless steel or space black stainless steel case, Sapphire crystal, Retina display with Force Touch, Ceramic back, Digital Crown, Heart rate sensor, accelerometer, and gyroscope, Ambient light sensor, Speaker and microphone, Wi-Fi (802.11b/g/n 2.4GHz), Bluetooth 4.0, Up to 18 hours of battery life*, Water resistant**, watchOS 2",
+        "subtitle 3": "What's in the Box:",
+        "info 3": "Apple Watch, Magnetic charging cable (2 m), USB power adapter (5W), Band (Sport Band includes band that can be configured for either S/M or M/L length), Quick start guide",
+        "footnote": "**Apple Watch is splash and water resistant but not waterproof."
+    ]
+
+    let images = [
+        ["thumbnail": "thumbnail 1"],
+        ["thumbnail": "thumbnail 2"],
+        ["thumbnail": "thumbnail 3"],
+        ["thumbnail": "thumbnail 4"],
+        ["thumbnail": "thumbnail 5"]
+    ]
+}
---
+++ StretchyHeader/StretchyHeader/DescriptionTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  DescriptionTableViewCell.swift
+//  EmbeddedCollectionView
+//
+//  Created by Ben Whitley on 7/19/17.
+//  Copyright © 2017 Ben Whitley. All rights reserved.
+//
+
+import UIKit
+
+class DescriptionTableViewCell: UITableViewCell {
+
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
